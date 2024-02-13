c15dde3c3c734b5f8225122953c42b203ec86d21
Muhammad Fachri Nuriza <fachri.nuriza@sg-dsa.com>
2024-02-13 11:10:50 +0700

    setup project

---
+++ mobile-app-pembayaran-qris/mobile-app-pembayaran-qris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile-app-pembayaran-qris
+//
+//  Created by Muhammad Fachri Nuriza on 13/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
